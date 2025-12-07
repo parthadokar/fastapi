a133e7e9fe2c4db2c8c3e36462e6f59040d18eef
parthadokar <adokar.parth@gmail.com>
2025-12-07 23:39:18 +0530

    modular structure

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from src.books.routes import book_router
+
+version = "v1"
+
+app = FastAPI(title="Book",version=version)
+
+app.include_router(book_router,prefix=f"/api/{version}/books",tags=['books'])
