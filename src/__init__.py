b5410ade359eaa196fb15039c4091df2f8621328
parthadokar <adokar.parth@gmail.com>
2025-12-08 23:19:14 +0530

    added db

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
 
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    print(f"Server is starting")
+    await init_db()
+    yield
+    print(f"Server has been stopped")
+    
 version = "v1"
 
-app = FastAPI(title="Book",version=version)
+app = FastAPI(title="Book",version=version,lifespan=lifespan)
 
 app.include_router(book_router,prefix=f"/api/{version}/books",tags=['books'])
